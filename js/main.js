0e6ae8976fdd9d3921463c95600cdff0e109ca74
tengkiatwong <tengkiatwong@gmail.com>
2017-04-02 20:11:49 +0800

    deploy

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('.element', {
+        strings: ["Responsive Websites.","Web Applications.","User Interfaces." ],
+        typeSpeed: 50,
+        backDelay: 750,
+        loop: true,
+        backSpeed: 80,
+        showCursor: false,
+      });
+  });
